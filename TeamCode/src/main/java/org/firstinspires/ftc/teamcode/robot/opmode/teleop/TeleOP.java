7314d39793dc0f523490db7de93c4ca93d5d9914
Medewerker <vksebastiaan@hotmail.com>
2026-04-14 15:36:21 +0200

    deleted TankDriveTrain2 zodat jesper weer verder kan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/teleop/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/teleop/TeleOP.java
@@ -3,23 +3,18 @@ package org.firstinspires.ftc.teamcode.robot.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.robot.subsystem.TankDriveTrain2;
 
 
 @TeleOp(name="TeleOP-DECODE-", group="Iterative Opmode")
 public class TeleOP extends OpMode {
     private final ElapsedTime runtime = new ElapsedTime();
 
-    private TankDriveTrain2 tankDrivetrain;
 
     @Override
     public void init() {
         telemetry.addData("Status", "Initializing");
 
-        tankDrivetrain = new TankDriveTrain2(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
     }
@@ -32,13 +27,6 @@ public class TeleOP extends OpMode {
 
     @Override
     public void loop() {
-        if ( Math.abs(gamepad1.left_stick_y) > 0.1){
-            tankDrivetrain.setDrivePower(gamepad1.left_stick_y);
-        } else if (Math.abs(gamepad1.right_stick_x) > 0.1) {
-            tankDrivetrain.setRotationPower(gamepad1.right_stick_x, -gamepad1.right_stick_x);
-        } else  {
-            tankDrivetrain.setDrivePower(0);
-        }
         telemetry.addData("Status", "Run Time: " + runtime.toString());
 
     }
