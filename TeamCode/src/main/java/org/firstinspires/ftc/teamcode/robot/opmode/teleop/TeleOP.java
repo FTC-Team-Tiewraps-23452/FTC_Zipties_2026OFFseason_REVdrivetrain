b8215488d68147d313be3a4ae46fb98ed6a2a2ca
NeroDisappeared <abigael.ramcharan@gmail.com>
2026-04-14 15:19:40 +0200

    Intake added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/teleop/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/teleop/TeleOP.java
@@ -4,21 +4,20 @@ package org.firstinspires.ftc.teamcode.robot.opmode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.robot.subsystem.MecanumDrivetrain;
+import org.firstinspires.ftc.teamcode.robot.subsystem.Intake;
 
 
 @TeleOp(name="TeleOP-IntoTheDeep-", group="Iterative Opmode")
 public class TeleOP extends OpMode {
     private final ElapsedTime runtime = new ElapsedTime();
+    private Intake intake;
 
-    private MecanumDrivetrain mecanumDrivetrain;
 
     @Override
     public void init() {
         telemetry.addData("Status", "Initializing");
 
-        mecanumDrivetrain = new MecanumDrivetrain(hardwareMap);
+        intake = new Intake(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
     }
@@ -36,19 +35,12 @@ public class TeleOP extends OpMode {
 
     @Override
     public void loop() {
-        //drivetrain
-      if (gamepad1.left_bumper){
-          double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
-          double x = gamepad1.left_stick_x;
-          double rx = -gamepad1.right_stick_x;
-          mecanumDrivetrain.mecanumDrive(x, y, rx);
-      } else {
-          double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
-          double x = gamepad1.left_stick_x;
-          double rx = -gamepad1.right_stick_x;
-          mecanumDrivetrain.mecanumDrive(x / 4,y / 4,rx / 4);
-      }
-
+    if (gamepad1.left_bumper){
+        intake.setIntakePower();
+    }
+    else {
+        intake.stop();
+    }
         telemetry.addData("Status", "Run Time: " + runtime.toString());
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystem/Intake.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.robot.subsystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+    private DcMotor intakeMotor;
+    public Intake (HardwareMap hardwareMap){
+        intakeMotor= hardwareMap.get(DcMotor.class,"intakeMotor");
+        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    public void setIntakePower(){
+        intakeMotor.setPower(0.7);
+    }
+    public void stop(){
+        intakeMotor.setPower(0);
+    }
+}
